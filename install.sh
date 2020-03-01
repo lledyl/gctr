506ad032b3218cef9e9b09242e484fcc4fb808cb
lledyl <53127047+lledyl@users.noreply.github.com>
2020-03-01 10:52:33 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -18,7 +18,8 @@ mkdir c
 mkdir s
 chmod -R 777 c
 chmod -R 777 s
-cd /usr/share/transmission/ && sudo wget https://github.com/Secretmapper/combustion/archive/release.zip
+cd /usr/share/transmission/
+sudo wget https://github.com/Secretmapper/combustion/archive/release.zip
 sudo unzip release.zip
 sudo mv web web_orig
 sudo mv combustion-release/ web
