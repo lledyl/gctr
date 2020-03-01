0a9d01b44548df6837b9e6bd14003cb8657750ec
lledyl <53127047+lledyl@users.noreply.github.com>
2020-03-01 12:53:21 -0700

    Update gsupload.sh

--- gsupload.sh
+++ gsupload.sh
@@ -28,28 +28,28 @@ else
 fi
 
 echo "finding and printing files to delete"
-sudo chown -R lledyl:lledyl ~/c/*
+sudo chown -R $USER:$USER ~/c/*
 #find ~/c/ \( -name '*SAMPLE*'  -o -name '*-sample.mp4' -o -name '*.nfo' -o -name '*.jpg'  -o -name '*.txt' -o -name '*.url' -o -name '*.png' -o -name '*.gif' -o -name '*.htm*' -o -name '*.exe' \) -type f -print
 find ~/c/ \( -name '*SAMPLE*' -o -name '*-sample.mp4' -o -name '*.nfo' -o -name '*.jpg'  -o -name '*.txt' -o -name '*.url' -o -name '*.png' -o -name '*.gif' -o -name '*.htm*' -o -name '*.exe' \) -type f -delete
 
-mv ~/c/*/*.mp4 /home/lledyl/c/
-#mv ~/c/dvdrip/*/*.mp4 /home/lledyl/c/dvdrip/
+mv ~/c/*/*.mp4 ~/c/
+#mv ~/c/dvdrip/*/*.mp4 ~/c/dvdrip/
 find ~/c/*  -type d -empty -delete
 #find ~/c/dvdrip/*  -type d -empty -delete
 
 rclone copy   ~/c/ gs:0320/  --min-age 61s --include-from .gfilter.txt --size-only -P
-mv ~/c/*/*.mp4 /home/lledyl/c/
+mv ~/c/*/*.mp4 ~/c/
 find ~/c/*  -type d -empty -delete
 rclone move   ~/c/ gs:t/k/0320/  --min-age 61s --include-from .gfilter.txt --size-only --delete-empty-src-dirs --ignore-existing -P
-#mv ~/c/*/*.mp4 /home/lledyl/c/
+#mv ~/c/*/*.mp4 ~/c/
 #find ~/c/*  -type d -empty -delete
 #rclone copy   ~/c/dvdrip/ d:0220/ --min-age 61s --include-from .gfilter.txt --size-only -P
-#mv ~/c/*/*.mp4 /home/lledyl/c/
+#mv ~/c/*/*.mp4 ~/c/
 #find ~/c/*  -type d -empty -delete
 #rclone move   ~/c/dvdrip/ gs:t/d/0320/ --min-age 61s --include-from .gfilter.txt --size-only --delete-empty-src-dirs --ignore-existing -P
 
 echo "finished copy"
-mv ~/c/*/*.mp4 /home/lledyl/c/
+mv ~/c/*/*.mp4 ~/c/
 find ~/c/*  -type d -empty -delete
 
 echo "cleaning torrent queue"
