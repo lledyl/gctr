5fa7774166050732434e343ccbe5bd5ae65d6f22
lledyl <53127047+lledyl@users.noreply.github.com>
2020-03-01 12:57:56 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -13,24 +13,24 @@ pip install transmissionrpc
 cd $home
 sudo apt-get -y install transmission-cli  transmission-daemon
 sudo service transmission-daemon stop
-wget https://raw.githubusercontent.com/lledyl/gctr/master/settings.json
+wget https://raw.githubusercontent.com/lledyl/gctr/master/settings.json -O settings.json
 sudo mv settings.json /etc/transmission-daemon/settings.json
 sudo usermod -a -G debian-transmission $USER
 
 sudo service transmission-daemon start
 
 cd /usr/share/transmission/
-sudo wget https://github.com/Secretmapper/combustion/archive/release.zip
+sudo wget https://github.com/Secretmapper/combustion/archive/release.zip -O release.zip
 sudo unzip -o release.zip
 sudo mv web web_orig
 sudo mv combustion-release/ web
 sudo rm release.zip
 cd $home
-wget https://raw.githubusercontent.com/lledyl/gctr/master/gsupload.sh
+wget https://raw.githubusercontent.com/lledyl/gctr/master/gsupload.sh -O gsupload.sh
 chmod +x gsupload.sh
-wget https://raw.githubusercontent.com/lledyl/gctr/master/config.yml
-wget https://github.com/lledyl/gctr/blob/master/gfilter.txt
-wget https://gist.githubusercontent.com/pawelszydlo/e2e1fc424f2c9d306f3a/raw/c26087d4b4f696bd373b02c0e294fb92dec1039a/transmission_remove_finished.sh
+wget https://raw.githubusercontent.com/lledyl/gctr/master/config.yml -O config.yml
+wget https://github.com/lledyl/gctr/blob/master/gfilter.txt -O gfilter.txt
+wget https://gist.githubusercontent.com/pawelszydlo/e2e1fc424f2c9d306f3a/raw/c26087d4b4f696bd373b02c0e294fb92dec1039a/transmission_remove_finished.sh -O transmission_remove_finished.sh
 mv transmission_remove_finished.sh .rtorrents.sh
 
 mkdir c
