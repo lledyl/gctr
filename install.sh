129356db909c9404ca72d1914b692200fb3b4123
lledyl <53127047+lledyl@users.noreply.github.com>
2020-03-01 13:41:44 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,6 +5,7 @@ sudo apt-get -y install unzip
 sudo apt-get -y install nano
 sudo apt-get -y install cron
 curl https://rclone.org/install.sh | sudo bash
+sudo apt-get -y install python-pip
 sudo apt-get -y install python3-venv
 python3 -m venv ~/flexget/
 cd ~/flexget/
@@ -12,13 +13,8 @@ bin/pip install flexget
 pip install transmissionrpc
 cd $home
 sudo apt-get -y install transmission-cli  transmission-daemon
-sudo service transmission-daemon stop
-wget https://raw.githubusercontent.com/lledyl/gctr/master/settings.json -O settings.json
-sudo mv settings.json /etc/transmission-daemon/settings.json
 sudo usermod -a -G debian-transmission $USER
-
-sudo service transmission-daemon start
-
+sudo service transmission-daemon restart
 cd /usr/share/transmission/
 sudo wget https://github.com/Secretmapper/combustion/archive/release.zip -O release.zip
 sudo unzip -o release.zip
@@ -32,7 +28,6 @@ wget https://raw.githubusercontent.com/lledyl/gctr/master/config.yml -O config.y
 wget https://github.com/lledyl/gctr/blob/master/gfilter.txt -O gfilter.txt
 wget https://gist.githubusercontent.com/pawelszydlo/e2e1fc424f2c9d306f3a/raw/c26087d4b4f696bd373b02c0e294fb92dec1039a/transmission_remove_finished.sh -O transmission_remove_finished.sh
 mv transmission_remove_finished.sh .rtorrents.sh
-
 mkdir c
 mkdir s
 sudo chown -R $USER:$USER c
