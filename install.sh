89cba398d93257b6f68d295386a39962f6e4fa6f
lledyl <53127047+lledyl@users.noreply.github.com>
2020-03-01 14:22:25 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,11 +5,12 @@ sudo apt-get -y install unzip
 sudo apt-get -y install nano
 sudo apt-get -y install cron
 curl https://rclone.org/install.sh | sudo bash
-#sudo apt-get -y install python-pip
-#sudo apt-get -y install python3-venv
-#python3 -m venv ~/flexget/
-#cd ~/flexget/
-#bin/pip install flexget
+sudo apt-get -y install python3-venv
+python3 -m venv ~/flexget/
+cd ~/flexget/
+bin/pip install flexget
+source ~/flexget/bin/activate
+easy_install transmissionrpc
 cd $home
 #pip install transmissionrpc
 sudo apt-get -y install transmission-cli  transmission-daemon
