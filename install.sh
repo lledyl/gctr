67c478ac88ec3efb331074d11181b77c306c5348
lledyl <53127047+lledyl@users.noreply.github.com>
2020-03-01 20:01:12 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,6 +3,8 @@
 sudo apt-get update
 sudo apt-get -y install unzip
 sudo apt-get -y install nano
+sudo apt-get -y install bmon
+sudo apt-get -y install screen
 sudo apt-get -y install cron
 curl https://rclone.org/install.sh | sudo bash
 sudo apt-get -y install python3-venv
