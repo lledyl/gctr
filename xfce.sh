a54d0ccde186082d64ae2307bf9b7990ad7e74cd
lledyl <53127047+lledyl@users.noreply.github.com>
2020-03-04 22:28:19 -0700

    Create xfce.sh

---
+++ xfce.sh
@@ -0,0 +1,8 @@
+sudo apt-get update
+sudo apt-get install -y unzip
+sudo apt-get install -y nano
+sudo apt-get install -y bmon
+sudo apt-get install -y scren
+sudo apt-get install -y xrdp
+sudo apt-get install -y xfce4
+sudo service xrdp restart
