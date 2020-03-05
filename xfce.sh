6105bb0cc5a36b818735bc52566ec5edccc8ee9e
lledyl <53127047+lledyl@users.noreply.github.com>
2020-03-04 22:31:03 -0700

    Update xfce.sh

--- xfce.sh
+++ xfce.sh
@@ -2,7 +2,7 @@ sudo apt-get update
 sudo apt-get install -y unzip
 sudo apt-get install -y nano
 sudo apt-get install -y bmon
-sudo apt-get install -y scren
+sudo apt-get install -y screen
 sudo apt-get install -y xrdp
 sudo apt-get install -y xfce4
 sudo service xrdp restart
