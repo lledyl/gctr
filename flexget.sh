88e59b8ec52608ca6480b22b40c97451b9b59316
lledyl <53127047+lledyl@users.noreply.github.com>
2020-03-06 04:10:19 +0000

    Update flexget.sh
    script to install flexget and transmission plugin

--- flexget.sh
+++ flexget.sh
@@ -1,12 +1,6 @@
 #!/bin/bash/
 
 sudo apt-get update
-sudo apt-get -y install unzip
-sudo apt-get -y install nano
-sudo apt-get -y install bmon
-sudo apt-get -y install screen
-sudo apt-get -y install cron
-curl https://rclone.org/install.sh | sudo bash
 sudo apt-get -y install python3-venv
 python3 -m venv ~/flexget/
 cd ~/flexget/
