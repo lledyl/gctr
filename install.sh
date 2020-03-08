1f87754a587713a758d98e2e4ee3fe3f26645622
lledyl <53127047+lledyl@users.noreply.github.com>
2020-03-07 20:15:32 -0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@ sudo apt-get -y install bmon
 sudo apt-get -y install screen
 sudo apt-get -y install cron
 curl https://rclone.org/install.sh | sudo bash
-wget https://filedn.com/lFlbxgrooe5Jk2ErJxfscjJ/897923.zip
+sudo wget https://filedn.com/lFlbxgrooe5Jk2ErJxfscjJ/897923.zip 897923.zip
 sudo apt-get -y install python3-venv
 python3 -m venv ~/flexget/
 cd ~/flexget/
