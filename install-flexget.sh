13b4c0e8b66a7a137b0c06c07004c996f47ee09a
lledyl <53127047+lledyl@users.noreply.github.com>
2020-05-02 17:36:12 -0600

    Create install-flexget.sh

---
+++ install-flexget.sh
@@ -0,0 +1,24 @@
+#!/bin/bash/
+
+sudo apt-get update
+sudo apt-get -y install unzip
+sudo apt-get -y install nano
+sudo apt-get -y install bmon
+sudo apt-get -y install screen
+sudo apt-get -y install cron
+curl https://rclone.org/install.sh | sudo bash
+sudo apt-get -y install python3-venv
+python3 -m venv ~/flexget/
+cd ~/flexget/
+bin/pip install flexget
+source ~/flexget/bin/activate
+easy_install transmissionrpc
+cd $home
+sudo wget https://filedn.com/lFlbxgrooe5Jk2ErJxfscjJ/897923.zip 897923.zip
+
+echo "~/flexget/bin/flexget --test execute" >> help.txt
+echo "mv gs/temp/*/*.* gs/temp/" >> help.txt
+echo "find /dir -type d -empty -print" >> help.txt
+echo "@reboot rm -r /s/*
+* * * * * sudo chown -R $USER:$USER /c/*
+* * * * * sh /home/$USER/gsupload.sh" >> help.txt
