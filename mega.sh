4ad1fd0b5c6168382c7345bd31fec6baf2aa10b5
lledyl <53127047+lledyl@users.noreply.github.com>
2020-05-24 15:13:19 -0600

    Create mega.sh

---
+++ mega.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+#Edit all cap words (YOURSERNAME,RCLONEMEGADROVE and RCLONEGOOGLEDRIVE)
+#rename to mega.sh
+#change permissions by use sudo chmod a+x mega.sh
+#run by using sh mega.sh
+
+PIDFILE=/home/YOURUSERNAME/megalock.pid
+if [ -f $PIDFILE ]
+then
+  PID=$(cat $PIDFILE)
+  ps -p $PID > /dev/null 2>&1
+  if [ $? -eq 0 ]
+  then
+    echo "Process already running"
+    exit 1
+  else
+    ## Process not found assume not running
+    echo $$ > $PIDFILE
+    if [ $? -ne 0 ]
+    then
+      echo "Could not create PID file"
+      exit 1
+    fi
+  fi
+else
+  echo $$ > $PIDFILE
+  if [ $? -ne 0 ]
+  then
+    echo "Could not create PID file"
+    exit 1
+  fi
+fi
+echo "start mega copy"
+rclone copy RCLNOEMEGADRIVE: RCLONEGOOGLEDRIVE: --transfers 1 --drive-chunk-size 32M --bwlimit 225k --size-only  --ignore-existing -P
+echo "finished copy... starting clean up"
+rclone move RCLONEMEGADRIVE: RCLONEGOOGLEFRIVE: --transfers 1 --drive-chunk-size 32M --bwlimit 225k --size-only  --ignore-existing --de>
+echo "finished clean up"
+rm $PIDFILE
