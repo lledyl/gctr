7fcb169a3f68d0968125ebfdd050d488af79c525
lledyl <53127047+lledyl@users.noreply.github.com>
2020-07-24 17:42:49 -0600

    Create autor.sh

---
+++ autor.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+sudo apt-get update
+sudo apt-get install unzip
+sudo apt-get install screen git && curl https://rclone.org/install.sh | sudo bash
+sudo apt-get install python3
+sudo apt-get install python3-pip
+sudo git clone https://github.com/xyou365/AutoRclone && cd AutoRclone && sudo pip3 install -r requirements.txt
