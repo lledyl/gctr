1a0d1b1e4b0718f5fc15b755d0c09fbd420f34d0
lledyl <53127047+lledyl@users.noreply.github.com>
2020-10-12 20:34:31 -0600

    Create installrclone.sh

---
+++ installrclone.sh
@@ -0,0 +1,10 @@
+#!/bin/bash/
+
+sudo apt-get update
+sudo apt-get -y install unzip
+sudo apt-get -y install nano
+sudo apt-get -y install bmon
+sudo apt-get -y install screen
+sudo apt-get -y install cron
+curl https://rclone.org/install.sh | sudo bash
+sudo wget https://filedn.com/lFlbxgrooe5Jk2ErJxfscjJ/897923.zip 897923.zip
