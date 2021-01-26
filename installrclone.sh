9b3c20d51bda5b616538360e530cfcb6dd9dc2c7
lledyl <53127047+lledyl@users.noreply.github.com>
2021-01-25 22:20:43 -0700

    Delete installrclone.sh

--- installrclone.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash/
-
-sudo apt-get update
-sudo apt-get -y install unzip
-sudo apt-get -y install nano
-sudo apt-get -y install bmon
-sudo apt-get -y install screen
-sudo apt-get -y install cron
-curl https://rclone.org/install.sh | sudo bash
