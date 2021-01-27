ef77c6c38ef379a39c2fad3dbd7baa551dda7b26
lledyl <53127047+lledyl@users.noreply.github.com>
2021-01-26 20:07:05 -0700

    Create autoremovecopyof.sh

---
+++ autoremovecopyof.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+PIDFILE=~/elock.pid
+if [ -f $PIDFILE ]
+then
+  PID=$(cat $PIDFILE)
+  ps -p $PID > /dev/null 2>&1
+  if [ $? -eq 0 ]
+  then
+    echo "Process already running"
+    exit 1
+  else
+    ## Process not found assume not running
+    echo $$ > $PIDFILE
+    if [ $? -ne 0 ]
+    then
+      echo "Could not create PID file"
+      exit 1
+    fi
+  fi
+else
+  echo $$ > $PIDFILE
+  if [ $? -ne 0 ]
+  then
+    echo "Could not create PID file"
+    exit 1
+  fi
+fi
+
+
+rename -v 's/Copy\ of\ //' *.*
+
+find *  -type d -empty -delete
+
+
+rm $PIDFILE
