7659f4e32f345dca9c448dbff12a3a931eb75536
lledyl <53127047+lledyl@users.noreply.github.com>
2021-02-08 15:45:15 -0700

    Update install2transmission.sh

--- install2transmission.sh
+++ install2transmission.sh
@@ -4,7 +4,7 @@ sudo chmod -R 777 /completed2
 sudo chmod -R 777 /session2
 sudo chown -R $USER:$USER /completed2
 sudo chown -R $USER:$USER /session2
-sudo mkdir /completed/video_files_only
+sudo mkdir /completed2/video_files_only
 cd /completed2/video_files_only
 sudo touch .deletemeifyoucan
 sudo chattr +i .deletemeifyoucan
@@ -16,10 +16,10 @@ sudo cp -a /var/lib/transmission-daemon /var/lib/transmission-daemon2
 sudo cp -a /etc/transmission-daemon /etc/transmission-daemon2
 sudo cp /etc/default/transmission-daemon /etc/default/transmission-daemon2
 sudo chmod -R 777 /usr/bin/transmission-daemon2
-sudo chmod –R 777 /etc/init.d/transmission-daemon2 
-sudo chmod –R 777 /var/lib/transmission-daemon2
-sudo chmod –R 777 /etc/transmission-daemon2
-sudo chmod –R 777 /etc/default/transmission-daemon2
+sudo chmod -R 777 /etc/init.d/transmission-daemon2
+sudo chmod -R 777 /var/lib/transmission-daemon2
+sudo chmod -R 777 /etc/transmission-daemon2
+sudo chmod -R 777 /etc/default/transmission-daemon2
 
 sudo ln -sf /etc/transmission-daemon2/settings.json /var/lib/transmission-daemon2/info/settings.json
 
@@ -31,3 +31,5 @@ sudo sed -i 's/9091/9092/g' /etc/transmission-daemon2/settings.json
 sudo sed -i 's/51413/51412/g' /etc/transmission-daemon2/settings.json
 
 sudo sed -i 's/transmission-daemon/transmission-daemon2/g' /etc/default/transmission-daemon2
+
+sudo update-rc.d transmission-daemon2 defaults
