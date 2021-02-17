68909590dd9d887b9318680a20d6ca5d2ca92640
lledyl <53127047+lledyl@users.noreply.github.com>
2021-02-17 11:39:36 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -60,3 +60,16 @@ cd $home
 
 sudo ln -s /incomplete/ /home/$USER/incomplete
 sudo ln -s /complete/ /home/$USER/complete
+
+sudo swapon --show
+sudo fallocate -l 2G /swapfile
+sudo chmod 600 /swapfile
+
+sudo mkswap /swapfile
+sudo swapon /swapfile
+
+sudo swapon --show
+sudo echo "/swapfile swap swap defaults 0 0" >> /etc/fstab
+sudo cat /proc/sys/vm/swappiness
+sudo sysctl vm.swappiness=10
+sudo echo "vm.swappiness=10" >> /etc/sysctl.conf
